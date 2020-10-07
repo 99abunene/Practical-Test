ea6650e87f499cda4d44ef0d98c2d9430f5cc478
kelxin tey <kelxin9806@hotmail.com>
2020-10-07 12:30:53 +0800

    changes

---
+++ Practical Test/Controller/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practical_Test
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Name, string PhoneNo, string Address, string CreatedAt)
+        {
+            return Content($"Hello {Name}\n"+ $"Your phone number is {PhoneNo}\n"+ $"Your address is {Address}\n"+ $"Your Create At {CreatedAt}\n");
+        }
+    }
+}
